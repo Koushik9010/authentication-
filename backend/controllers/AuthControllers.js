d794ef340830d8f8fd5ee52a8194558934f7037e
Koushik9010 <anikdey.6365@gmail.com>
2025-02-02 20:45:21 +0600

    Done login controller

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -109,7 +109,45 @@ export const VerifyEmailController = async (req, res) => {
 
 //login controller
 export const LoginController = async (req, res) => {
-  res.send("LoginController");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login controller", error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 //logout controller
